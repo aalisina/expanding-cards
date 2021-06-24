2997fdc77fb720e97ad81b0247002401a9eed6d5
sinazay123 <sinazay123@gmail.com>
2021-06-24 18:47:54 -0500

    last changes

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// put elements in a nodelist
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach((panel)=> {
-    panel.addEventListener('click', ()=> {
-        
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
     })
 })
 
-function removeActiveClasses(){
+function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
     })
